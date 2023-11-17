44308fad3c12843fa5903d5a099a73aae3593cca
aluno <106314933+acougueirodecanela09@users.noreply.github.com>
2023-11-17 09:34:47 -0300

    final

--- jogos.js
+++ jogos.js
@@ -9,8 +9,20 @@ JOGADOR_O = "O";
 
 let jogoAcabou = false;
 
-document.addEventListener("click", (event) =>) {
+document.addEventListener("click", (event) => {
     if (event.target.matches(".celula") && !jogoAcabou) {
-        jogar(event.target.id):
+        jogar(event.target.id);
     }
-}):
+});
+
+functionjogar(id) {
+    const celula = document.getElementById(id);
+    if (celula.textContent ==="") {
+        turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+        checarTurno = !checarTurno;
+        celula.textContent = turno;
+        celula.classList.add(turno); 
+        celula.removeEventListener("click, jogar");
+        verificarVencedor(turno);
+    }
+}
